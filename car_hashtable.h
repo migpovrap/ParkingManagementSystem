cd443b4ae7489c895b83ada30e45123cec2b08a8
Miguel Raposo <migpovrap@hotmail.com>
2024-03-29 12:32:31 +0000

    correções

--- car_hashtable.h
+++ car_hashtable.h
@@ -9,7 +9,7 @@ Car* search_car_hashtable (Car* cars[], char mt[], int size);
 void add_car_to_end_list (Car** list_head, Car* new_car);
 void clear_logcars_list (Car** list_head);
 Car* search_logcars_list (char mt[], Car** list_head);
-
+Car* remove_car_hashtable(int parknumber, ParksData* parksdata, char mt[9]);
 
 //Fazer função para por carros no final de uma linkedlist
 
