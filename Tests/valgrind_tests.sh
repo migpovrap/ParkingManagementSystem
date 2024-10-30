5ac16bf91dfa11f93e61d5c17b72c68a2ac486a7
Miguel Raposo <migpovrap@hotmail.com>
2024-10-30 11:00:21 +0000

    Fix valgrind log

--- Tests/valgrind_tests.sh
+++ Tests/valgrind_tests.sh
@@ -5,15 +5,17 @@ TEST_DIR="private"
 
 # Executable to be tested
 EXE="./proj1"
+
 # Log file to save valgrind output
 LOG_FILE="valgrind_output.log"
+
 # Clear the log file if it exists
 > "$LOG_FILE"
 
 # Loop through all .in files in the tests directory
 for test_file in "$TEST_DIR"/*.in; do
    echo "Running valgrind for $test_file"
-   valgrind "$EXE" < "$test_file" >> "$LOG_FILE" 2>&1
+   valgrind "$EXE" < "$test_file" > /dev/null 2>> "$LOG_FILE"
 done
 
 echo "All tests completed. Valgrind output saved to $LOG_FILE."
